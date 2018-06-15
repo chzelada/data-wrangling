a8688f54d7e5d3def43e00c060757ff0adbeef6e
czelada <czelada@healthcare.com>
2018-06-15 10:40:06 -0600

    Week 4 start

--- week4/SpotifyAPIcalls.R
+++ week4/SpotifyAPIcalls.R
@@ -1,5 +1,6 @@
 #### References
 #### http://r4ds.had.co.nz/lists.html
+#### https://jennybc.github.io/purrr-tutorial/ls01_map-name-position-shortcuts.html
 
 
 library(httr)
@@ -21,24 +22,24 @@ my_token <- httr::content(httr::POST('https://accounts.spotify.com/api/token', m
 #### Find Artist ####
 auth_header <- httr::add_headers('Authorization'= paste('Bearer',my_token$access_token))
 url <- "https://api.spotify.com/v1/search"
-artist <- "Bohemia"
+artist <- URLencode("Luis Miguel")
 artist_info <- httr::content( httr::GET(paste(url,'?q=',artist,'&type=artist',sep=''),auth_header))
 
-artist_info$artists$total
-map_chr(artist_info$artists$items,"name")
+map_chr(artist_info$artists$items,c("name"))
 map(artist_info$artists$items,"followers") %>% map_int("total")
-
+map(artist_info$artists$items,"genres") %>% map_chr(paste,collapse=",")
 
 #### Find Track ####
 
 url <- "https://api.spotify.com/v1/search"
-artist <- "Despacito"
+artist <- URLencode("Luis Fonsi")
 track_info <- httr::content( httr::GET(paste(url,'?q=',artist,'&type=track&market=US',sep=''),auth_header))
 
 track_info$tracks$items[[1]]$id
 artist_info$artists$items[[1]]$name
 artist_info$artists$items[[1]]$genres
 artist_info$artists$items[[1]]$id
+
 
 ### Audio Feature ###
 auth_header <- httr::add_headers('Authorization'= paste('Bearer',my_token$access_token))
