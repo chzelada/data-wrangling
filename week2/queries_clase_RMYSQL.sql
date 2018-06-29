c9e99d19beae69099670cbf3ed2efc25b0004914
Juan Carlos Girón <jcgiron@ufm.edu>
2018-06-29 15:27:42 -0600

    Finished RMySQL class, added an SQL file with queries.

---
+++ week2/queries_clase_RMYSQL.sql
@@ -0,0 +1,11 @@
+ EXPLAIN SELECT * FROM heroes_information limit 10;
+ 
+ show tables;
+ show columns from heroes_information;
+ describe heroes_information;
+ 
+ Select * from  heroes_information limit 10;
+ 
+ Select count(*) from heroes_information;
+ 
+drop table test;
